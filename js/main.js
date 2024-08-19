a23e22b3b8feb3e75bf38b387581e80375b6a691
Mina-Emad-20 <167125155+Mina-Emad-20@users.noreply.github.com>
2024-08-19 22:05:05 +0300

    marketing-web

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// console.log("HELLO JavaScript")
+
+// var x = 5;
+// console.log(x)
+
+// var y = 8;
+// console.log(y)
+
+/*
+var z = x + y;
+console.log(z)
+var firstname = prompt("please enter your first name")
+var UserlastName = prompt("please enter your last name")
+var userAge = prompt("please enter your age")
+
+
+console.log(firstname + " " + UserlastName + " " + userAge)
+*/
+
+// var userAge = +prompt("enter your age")
+// console.log("your agr in days" + " " + userAge * 365)
